8d497b78572932ee470a49b90f21c264b2403eb3
akm250 <65080274+akm250@users.noreply.github.com>
2020-08-17 22:08:50 +0530

    17 Aug

---
+++ js/tut6.js
@@ -0,0 +1,32 @@
+console.log('We are tut6')
+const name = 'Alok';
+const greeting = 'Good Morning';
+// console.log(greeting + ' ' + name);
+
+let html;
+html = "<h1> this is headig<h>"+"<p> this is my para<p>";
+
+html = html.concat('this', 'str2');
+// console.log(html);
+// console.log(html.length);
+// console.log(html.toLowerCase());
+// console.log(html.toUpperCase());
+// console.log(html);
+
+// console.log(html[1]);
+// console.log(html.indexOf('<'));
+// console.log(html.lastIndexOf('<'));
+// console.log(html.charAt('3'));
+// console.log(html.endsWith('str2'));
+// console.log(html.includes('if'));
+// console.log(html.substring(1,6));
+// console.log(html.slice(0,4));
+// console.log(html.split(' '));
+// console.log(html.replace('this', 'it'));
+
+let fruit1 = 'Orange\'';
+let fruit2 = 'Apple';
+let myHtml = `Hello ${name}
+             <h1> This is "my" heading <h1>
+             <p> You like ${fruit1} and ${fruit2} `;
+document.body.innerHTML = myHtml;
