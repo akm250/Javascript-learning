04d4e504797629d6edad4607755b06d195d59f08
akm250 <65080274+akm250@users.noreply.github.com>
2020-08-19 20:18:04 +0530

    19 Aug

---
+++ js/tut10.js
@@ -0,0 +1,44 @@
+console.log('this is tut 10');
+
+if(1){
+    let i =234;
+    console.log(i)
+}
+
+console.log(i);
+
+function ui(name)
+{
+    let i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+}
+
+console.log(ui("Alok"), i)
+// const mygreet = function(name, thank='Thank You'){
+//     let msg = `Happy Birthday ${name} Wishing you a day filled with happiness and a year filled with joy. ${thank}!`;
+//     return msg;
+
+// }
+
+
+// let name = 'SkillF';
+// let name2 ='Rohan';
+
+// let val = mygreet(name, 'Thanks a lot');
+// console.log(val);
+
+
+// const myobj = {
+//     name: "SkillF",
+//     game: function(){
+//         return "GTA";
+//     }
+// }
+// console.log(myobj.game())
+
+// arr = ['fruit', 'vegetable', 'furniture'];
+
+// arr.forEach(function(element, index, array) {
+//     console.log(element, index)
+// });         
